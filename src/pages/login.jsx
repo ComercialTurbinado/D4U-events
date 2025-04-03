3d1e4c03e77cbf98859c318299200fe2e5b968f7
Claudio Damasco <claudiodamasco@Mac-Studio-de-Claudio.local>
2025-04-03 16:40:29 -0300

    ajuste login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -53,14 +53,9 @@ export default function LoginPage() {
       if (response.ok) {
         localStorage.setItem("token", data.token);
         localStorage.setItem("user", JSON.stringify(data.user));
-        localStorage.setItem("user_id", JSON.stringify(data.user._id));
-        localStorage.setItem("user_name", JSON.stringify(data.user.name));
-        localStorage.setItem("user_position", JSON.stringify(data.user.position));
-        localStorage.setItem("user_department_id", JSON.stringify(data.user.department_id));
+        console.log(data);
+        console.log(response);
         
-        console.log(localStorage.getItem("user_name"));
-        console.log(localStorage.getItem("user_department_id"));
-
          navigate("/");
       } else {
         console.error("Resposta com erro:", data);
